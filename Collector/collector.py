059647b7c496072ab1894c5b81eeef42c91afb40
Bill Reyor <87031733+BillReyor@users.noreply.github.com>
2022-01-30 08:24:35 -0500

    Update collector.py

--- Collector/collector.py
+++ Collector/collector.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 # requires pyshark https://github.com/KimiNewt/pyshark/ (wrapper for the wireshark - tshark tool)
-# example usage here: https://github.com/xdavidhu/probeSniffer 
+# example usage here: https://github.com/xdavidhu/probeSniffer (now defunct and failing build)
 
 #!
